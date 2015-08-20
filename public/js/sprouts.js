121ac9b5c9915c9617d4110f8494843bc532a1ba
SasquatchShawn <srmiranda1@gmail.com>
2015-08-20 14:07:14 -0400

    Working with extra credit of scrolling

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,14 +1,20 @@
+
+//
+
+// Old Way with Link
+
+// $('.more-sprouts').on("click", function (event) {
+//   event.preventDefault();
+
 var nextPage = 2
 
-$('.more-sprouts').on("click", function (event) {
-  event.preventDefault();
-
+$(window).scroll(function() {
+  if($(window).scrollTop() == $(document).height() - $(window).height()) {
   $.get("/tweets.json?page=" + (nextPage), function(data) {
     nextPage++;
-    // data["tweets"].forEach(function(tweet) {
-    //   $("#tweet-list").append("<li>" + tweet + "</li>");
     data.forEach(function(tweet) {
       $(".tweets").append("<li class='tweet'><div class='body'>" + tweet.text + "</div><div class='user'>" + tweet.username + "</div></li>");
     });
   });
+ }
 });
